49f75e8a75a970b32a8c2f62d8958f6a20be9af0
Priyanshu9898 <priyanshu.mce20@sot.pdpu.ac.in>
2024-06-05 22:32:14 +0530

    api bug  fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "./_components/HeroSection";
 import FeatureSection from "./_components/FeatureSection";
+import Testimonials from "./_components/Testimonials";
 
 const page = () => {
   return (
     <>
       <HeroSection />
       <FeatureSection />
+      <Testimonials />
     </>
   );
 };
